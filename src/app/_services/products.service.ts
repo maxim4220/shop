bd39a3a98447e036aa78a2879a19cf839be8febc
Maxim B <maxim4220@gmail.com>
2019-09-22 21:00:02 +0300

    Added products service

---
+++ src/app/_services/products.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+
+    /**
+     * Get the list of products..
+     */
+    public getProducts() {
+        const API = 'http://smktesting.herokuapp.com/api/products/';
+        return this.http.get(API);
+    }
+
+    public getReviews(productId) {
+        let api = 'http://smktesting.herokuapp.com/api/reviews/' + productId;
+        return this.http.get(api);
+    }
+  
+}
