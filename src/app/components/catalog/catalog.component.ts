a70b165dac7f5fa63399cd9188ef986200e0928a
Maxim B <maxim4220@gmail.com>
2019-09-22 20:58:09 +0300

    Added register component with base logic

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../_services/products.service';
+import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
   selector: 'app-catalog',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent implements OnInit {
+  // list of products.
+  public products:any = [];
+  public showDetails = false;
+  public singleProduct;
+  // reviews of selected product.
+  public reviews;
+  public showSpinner: boolean = true;
+  currentUser: any;
 
-  constructor() { }
+  constructor(private productsService: ProductsService,  private authenticationService: AuthenticationService) { 
+    this.authenticationService.isLogedIn.subscribe(x => this.currentUser = x);
+  }
 
   ngOnInit() {
+    console.log('Catalog runs!!');
+    console.log('currentUser catalog', this.currentUser);
+    
+    this.productsService.getProducts().subscribe((response) => {
+      if(response) {
+        this.products = response;
+        console.log('this.products', this.products);
+        this.showSpinner = false;
+      }
+     });
+  }
+
+  public showProductDetails(product) {
+    this.showDetails = true;
+    this.singleProduct = product;
+    this.showReviews(product.id);
+  }
+
+  private showReviews(id) {
+   this.productsService.getReviews(id).subscribe((response) => {
+  if(response) {
+   console.log('reviews', response);
+   this.reviews = response;
+  }
+ });
   }
 
 }
+
