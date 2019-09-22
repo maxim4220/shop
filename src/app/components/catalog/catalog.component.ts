a6ddea6750dcf52d2170ee32d5afa510d21eac66
Maxim B <maxim4220@gmail.com>
2019-09-23 00:34:04 +0300

    Added logic for the catalog component to calculate average rating for products.

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../_services/products.service';
 import { AuthenticationService } from '../../_services/authentication.service';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-catalog',
@@ -17,36 +18,67 @@ export class CatalogComponent implements OnInit {
   public showSpinner: boolean = true;
   currentUser: any;
 
-  constructor(private productsService: ProductsService,  private authenticationService: AuthenticationService) { 
+  constructor(private productsService: ProductsService,  private authenticationService: AuthenticationService,config: NgbRatingConfig) { 
+    config.max = 5;
+    config.readonly = true;
     this.authenticationService.isLogedIn.subscribe(x => this.currentUser = x);
   }
 
   ngOnInit() {
-    console.log('Catalog runs!!');
-    console.log('currentUser catalog', this.currentUser);
-    
+   
+  //  console.log('currentUser catalog', this.currentUser);
+    // Get catalog products.
     this.productsService.getProducts().subscribe((response) => {
       if(response) {
         this.products = response;
         console.log('this.products', this.products);
+        // Send requests for product reviews.
+        this.products.forEach(element => {
+         this.showReviews(element.id);
+        });
         this.showSpinner = false;
       }
      });
   }
 
   public showProductDetails(product) {
-    this.showDetails = true;
-    this.singleProduct = product;
-    this.showReviews(product.id);
+   // this.showDetails = true;
+    //this.singleProduct = product;
+   //  this.showReviews(product.id);
   }
-
-  private showReviews(id) {
-   this.productsService.getReviews(id).subscribe((response) => {
+  
+  // Send request for products reviews 
+  private showReviews(product_id) {
+   this.productsService.getReviews(product_id).subscribe((response) => {
   if(response) {
-   console.log('reviews', response);
-   this.reviews = response;
+     this.reviews = response;
+     this.calculateAverageRate(this.reviews, product_id);
+  
   }
  });
+  }
+
+  // Calcurale average rate for each product based on all existing revies.
+  private calculateAverageRate(reviews, product_id) {
+    let total = 0;
+    let counter = 0;
+    reviews.forEach(element => {
+      total += element.rate;
+      if(element.rate > 0) {
+        counter ++;
+      }
+    });
+  
+   let res = this.products.find(x => x.id == product_id);
+    console.log('res', res);
+    if(res) {
+      // Add average rate to products and round it to the larger integer (p.s. like many web markets do)
+      res = Object.assign(res, {averageRate: Math.ceil(total / counter)}, {totalComents: reviews.length});
+      console.log('res222', res);
+      console.log('prodsss', this.products);
+      
+    }
+    
   }
 
 }
