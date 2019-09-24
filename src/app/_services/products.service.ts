7578ad4c4a0aaf5301f7555529f3c30186f6802b
Maxim B <maxim4220@gmail.com>
2019-09-24 03:11:51 +0300

    Added comments

--- src/app/_services/products.service.ts
+++ src/app/_services/products.service.ts
@@ -8,8 +8,7 @@ export class ProductsService {
 
   constructor(private http: HttpClient) {
   }
-
-
+  
   /**
    * Get the list of products..
    */
@@ -18,15 +17,24 @@ export class ProductsService {
     return this.http.get(API);
   }
 
+  /**
+   * Get reviews of products
+   * @param productId: Id of the product
+   */
   public getReviews(productId) {
     const API = API_DOMAIN +'reviews/' + productId;
     return this.http.get(API);
   }
 
+   /**
+   * Add review for the single product
+   * @param productId: Id of the product
+   * @param rate: product rate
+   * @param text: review
+   */
   public addReview(productId, rate, text) {
     const API = API_DOMAIN + 'reviews/'+ productId;
     return this.http.post(API, { rate, text});
-  
   }
 
 }
