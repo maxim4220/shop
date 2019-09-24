7c4796a786e17747254434aaa10fb8521aa275d8
Maxim B <maxim4220@gmail.com>
2019-09-24 09:33:05 +0300

    Removed console

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -9,11 +9,8 @@ export class JwtInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthenticationService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('Interceptor called!!');
-        
         // add authorization header with jwt token if available
         let currentUserToken = this.authenticationService.currentUserValue;
-        console.log('currentUserToken!!', currentUserToken);
         if (currentUserToken ) {
             request = request.clone({
                 setHeaders: { 
